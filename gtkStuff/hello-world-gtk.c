0b1067bf14dbe9bab30d01a586f8e6d5171f013c
Maiku <realmaiku@gmail.com>
2024-11-25 19:44:00 -0500

    Adjusted window size, added app ID

--- gtkStuff/hello-world-gtk.c
+++ gtkStuff/hello-world-gtk.c
@@ -1,27 +1,14 @@
 #include <gtk/gtk.h>
 
 static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
-}
-
-static void
-activate (GtkApplication *app,
+activate (GtkApplication* app,
           gpointer        user_data)
 {
   GtkWidget *window;
-  GtkWidget *button;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Hello");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
-
+  gtk_window_set_title (GTK_WINDOW (window), "Calculator");
+  gtk_window_set_default_size (GTK_WINDOW (window), 500, 900);
   gtk_window_present (GTK_WINDOW (window));
 }
 
@@ -32,7 +19,7 @@ main (int    argc,
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new ("com.github.methodmen", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
