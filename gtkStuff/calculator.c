d4651d01b89ec3df5ec67d5cb39b158a2aa9fb3e
Behzad Janjua <behzadjanjua@Behzads-MacBook-Pro.local>
2024-11-26 18:38:12 -0500

    Added subtraction feature

--- gtkStuff/calculator.c
+++ gtkStuff/calculator.c
@@ -2,32 +2,35 @@
 #include <glib/gstdio.h>
 
 /*
- * Global variables that will be used to store the input sequence from the calculator 
+ * Global variables that will be used to store the input sequence from the calculator
  * userInput is initialized within the main() function
-*/ 
+ */
 char *userInput;
 int currentIndex = 0;
 
-
 // Returns a string pointer to an allocated block of memory 100 bytes long
-char *string_memory_allocate() {
+char *string_memory_allocate()
+{
   char *inputString = calloc(100, sizeof(char));
-  if (inputString == NULL) {
+  if (inputString == NULL)
+  {
     fprintf(stderr, "Not enough memory to store string input\n");
     exit(EXIT_FAILURE);
   }
-  
+
   return inputString;
 }
 
 // Stores characters in string at currentIndex position
-static void store_input_value(char value_to_store) { 
+static void store_input_value(char value_to_store)
+{
   userInput[currentIndex] = value_to_store;
   currentIndex++;
 }
 
 // Allocates a new string pointer using string_memory_allocate(), frees old pointer
-char *reset_userInput(char *inputString) {
+char *reset_userInput(char *inputString)
+{
   char *resetString = string_memory_allocate();
   free(inputString);
 
@@ -35,9 +38,10 @@ char *reset_userInput(char *inputString) {
   return resetString;
 }
 
-static void on_digit_click(GtkButton *button, gpointer user_data) {
+static void on_digit_click(GtkButton *button, gpointer user_data)
+{
   int digit = GPOINTER_TO_INT(user_data); // Retrieve digit input from pointer
-  store_input_value(digit+'0'); // Store digit in input string
+  store_input_value(digit + '0');         // Store digit in input string
 
   g_print("The current input index location is: %d\n", currentIndex);
   g_print("Button %d was pressed!\n", digit);
@@ -45,54 +49,66 @@ static void on_digit_click(GtkButton *button, gpointer user_data) {
 }
 
 // Resizes the string to minimal necessary byte size
-char *string_reallocate(char *stringInput) {
-  char *resizedString = realloc(stringInput, strlen(userInput)+1);
-  
+char *string_reallocate(char *stringInput)
+{
+  char *resizedString = realloc(stringInput, strlen(userInput) + 1);
+
   return resizedString;
 }
 
 // Acts as an input sequence break, processes' input and then resets it for next input sequence
-static void on_equal_button_clicked(GtkButton *button, gpointer user_data) {
-    g_print("The equal button was pushed!\n");
-    
-    userInput = string_reallocate(userInput);
-    g_printf("%s\n", userInput);
-    g_printf("Size of string: %ld\n", strlen(userInput));
-    userInput = reset_userInput(userInput);
-    g_print("\n");
+static void on_equal_button_clicked(GtkButton *button, gpointer user_data)
+{
+  g_print("The equal button was pushed!\n");
+
+  userInput = string_reallocate(userInput);
+  g_printf("%s\n", userInput);
+  g_printf("Size of string: %ld\n", strlen(userInput));
+  userInput = reset_userInput(userInput);
+  g_print("\n");
 }
 
-static void on_add_button_clicked(GtkButton *button, gpointer user_data) {
-    g_print("The add button was pushed!\n");
+static void on_add_button_clicked(GtkButton *button, gpointer user_data)
+{
+  g_print("The add button was pushed!\n");
 
-    store_input_value('+');
+  store_input_value('+');
 }
 
+static void on_subtract_button_clicked(GtkButton *button, gpointer user_data)
+{
+  g_print("The subtract button was pushed!\n");
+
+  store_input_value('-');
+}
 
 // Help callback function, outputs application instructions to whatever terminal was used to launch the application
-static void on_help_button_clicked(GtkButton *button, gpointer user_data) {
-    g_print("This app is in very early development, and only contains the basic calculator functions\n");
-    g_print("These functions include: addition and subtraction.\nIn order to properly operate this calculator, follow these steps:\n");
-    g_print("1: Enter the first number you would like to add, or subtract from\n2: Select the add or subtract operator\n3: Enter the second number you are adding with the first, or subtracting from the first\n");
-    g_print("4: Press the equals button to show the sum/difference\n");
+static void on_help_button_clicked(GtkButton *button, gpointer user_data)
+{
+  g_print("This app is in very early development, and only contains the basic calculator functions\n");
+  g_print("These functions include: addition and subtraction.\nIn order to properly operate this calculator, follow these steps:\n");
+  g_print("1: Enter the first number you would like to add, or subtract from\n2: Select the add or subtract operator\n3: Enter the second number you are adding with the first, or subtracting from the first\n");
+  g_print("4: Press the equals button to show the sum/difference\n");
 }
 
-static void activate (GtkApplication *app, gpointer user_data) {
+static void activate(GtkApplication *app, gpointer user_data)
+{
   /* Construct a GtkBuilder instance and load our UI description */
-  GtkBuilder *builder = gtk_builder_new ();
-  gtk_builder_add_from_file (builder, "builder.ui", NULL);
+  GtkBuilder *builder = gtk_builder_new();
+  gtk_builder_add_from_file(builder, "builder.ui", NULL);
 
   /* Connect signal handlers to the constructed widgets. */
-  GObject *window = gtk_builder_get_object (builder, "window");
-  gtk_window_set_application (GTK_WINDOW (window), app);
+  GObject *window = gtk_builder_get_object(builder, "window");
+  gtk_window_set_application(GTK_WINDOW(window), app);
 
   // Creates buttons 0 through 9, connects them to the on_digit_click() function
   // builder.ui xml is where each button is "styled"
-  for (int currentDigit = 0; currentDigit <= 9; currentDigit++) {
+  for (int currentDigit = 0; currentDigit <= 9; currentDigit++)
+  {
     char currentButton[10];
     sprintf(currentButton, "button%d", currentDigit);
 
-    GObject *button = gtk_builder_get_object (builder, currentButton);
+    GObject *button = gtk_builder_get_object(builder, currentButton);
     g_signal_connect(button, "clicked", G_CALLBACK(on_digit_click), GINT_TO_POINTER(currentDigit));
   }
 
@@ -105,26 +121,28 @@ static void activate (GtkApplication *app, gpointer user_data) {
   GObject *addition_button = gtk_builder_get_object(builder, "button_add");
   g_signal_connect(addition_button, "clicked", G_CALLBACK(on_add_button_clicked), NULL);
 
+  GObject *subtraction_button = gtk_builder_get_object(builder, "button_subtract");
+  g_signal_connect(subtraction_button, "clicked", G_CALLBACK(on_subtract_button_clicked), NULL);
 
-  gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
+  gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
 
   /* We do not need the builder any more */
-  g_object_unref (builder);
+  g_object_unref(builder);
 }
 
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   userInput = string_memory_allocate(); // Initialize the input string
 
-  #ifdef GTK_SRCDIR
-    g_chdir (GTK_SRCDIR);
-  #endif
+#ifdef GTK_SRCDIR
+  g_chdir(GTK_SRCDIR);
+#endif
 
-  GtkApplication *calculatorApp = gtk_application_new ("com.github.methodmen", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (calculatorApp, "activate", G_CALLBACK (activate), NULL);
+  GtkApplication *calculatorApp = gtk_application_new("com.github.methodmen", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(calculatorApp, "activate", G_CALLBACK(activate), NULL);
 
-  int status = g_application_run (G_APPLICATION (calculatorApp), argc, argv);
-  g_object_unref (calculatorApp);
+  int status = g_application_run(G_APPLICATION(calculatorApp), argc, argv);
+  g_object_unref(calculatorApp);
 
   free(userInput);
   return status;
