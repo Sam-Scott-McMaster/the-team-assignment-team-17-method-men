ff36a2f85b4f68b9e17487396f9c0571cdd9bc76
benbloomie <bloomfib@mcmaster.ca>
2024-12-04 15:36:38 -0500

    correcting expected output for test script.

--- test_script.sh
+++ test_script.sh
@@ -116,13 +116,13 @@ test './cmain' 0 "0" 'Final result: 0.00' ''
 test './cmain' 0 "3-7" 'Final result: -4.00' ''
 
 # 11. more complex parentheses
-test './cmain' 0 "((2+3)*(4-1))+(5*(6/2))" 'Final result: 30' ''
+test './cmain' 0 "((2+3)*(4-1))+(5*(6/2))" 'Final result: 30.00' ''
 
 # 12. more complex parentheses
-# test './cmain' 0 "((8-3)/(2+1))*(7-(3+2))" 'Final result: 3.34' ''
+test './cmain' 0 "((8-3)/(2+1))*(7-(3+2))" 'Final result: 3.34' ''
 
 # 13. more complex parentheses
-# test './cmain' 0 "(12/(3+(5-2)))+((6*2)-(4/2))" 'Final result: 12' ''
+test './cmain' 0 "(12/(3+(5-2)))+((6*2)-(4/2))" 'Final result: 12.00' ''
 
 # 14. negative number input
 # test './cmain' 0 "-1+3" 'Final result: 2' ''
